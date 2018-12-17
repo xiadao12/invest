048f202a3a5a3a48dad8eb809c03eb16e7eeef6f
xiadao12 <xiadao12@yeah.net>
2018-12-17 14:33:02 +0800

    修改

---
+++ src/main/java/com/zcy/invest/core/SecretContent.java
@@ -0,0 +1,15 @@
+package com.zcy.invest.core;
+
+/**
+ * 安全内容，不提交
+ *
+ * @version 1.0.0
+ * @brief ecs insight
+ * @note 修订历史： 1、yangzhouchuan于2018/12/17设计并构建初始版本v1.0.0
+ */
+public class SecretContent {
+
+    public final static String IQOPTION_USERNAME = "851883560@qq.com";
+    public final static String IQOPTION_Password =
+
+}
